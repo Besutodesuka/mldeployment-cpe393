2f559b21a7157fde004c781951b697b6ca7c576b
Rapepong Pitijaroonpong <besutodesuka@gmail.com>
2025-03-27 21:58:45 +0700

    remove unnneccessary function

--- iris/app/app.py
+++ iris/app/app.py
@@ -47,6 +47,16 @@ def predict_prob():
                     "confidence":float(prediction[0,cls])
                     })
 
+# @app.route("/predict_prob", methods=["POST"])
+# def predict_prob():
+#     data = request.get_json()
+#     input_features = np.array(data["features"]).reshape(len(data["features"]), -1)
+#     prediction = model.predict_proba(input_features)
+#     cls = np.argmax(prediction, axis = -1)
+#     return jsonify({"prediction":cls.tolist(),
+#                     "confidence":prediction[:,cls].tolist()
+#                     })
+
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify(status="ok")
